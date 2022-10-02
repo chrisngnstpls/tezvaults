8ff6f845392a5e92a4af2efd58b45c6f981e0f5d
chrisngnstpls <chrisngnstpls@gmail.com>
2022-10-02 23:42:31 +0300

    axios update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { CreateVault } from "./CreateVault";
 import { FirstTime } from "./FirstTime";
+import { Topbar } from "./Topbar";
 
 export const Main = () => {
 
   return (
+    <section>
     <main>
         <CreateVault />
     </main>
+    </section>
   );
 };
